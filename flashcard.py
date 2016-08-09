6bbf43a9137881cf650e1851bf2ec5f411e44f86
Joe <jweber4213@gmail.com>
2016-08-10 00:19:33 +0900

    removed bloat

--- flashcard.py
+++ flashcard.py
@@ -10,31 +10,30 @@ f1content = file1.readlines()
 f2content = file2.readlines()
 
 while count < 10:
-	os.system('clear')
-	
-	wordnum = random.randint(0, len(f1content)-1)
-
-	print 'Word:', f1content[wordnum], ''
+    os.system('clear')
+    wordnum = random.randint(0, len(f1content)-1)
+    print 'Word:', f1content[wordnum], ''
 
     options = [random.randint(0, len(f2content)-1),
         random.randint(0, len(f2content)-1),
         random.randint(0, len(f2content)-1)]
 
-	options[random.randint(0, 2)] = wordnum
+    options[random.randint(0,2)] = wordnum
 
     print '1 -', f2content[options[0]],
     print '2 -', f2content[options[1]],
     print '3 -', f2content[options[2]],
 
-	answer = input('\nYour choice: ')
+    answer = input('\nYour choice: ')
 
-	if options[answer-1] == wordnum:
-		raw_input('\nCorrect! Hit enter...')
-		score = score+1
-	else:
-		raw_input('\nWrong! Hit enter...')
+    if options[answer-1] == wordnum:
+        raw_input('\nCorrect! Hit enter...')
+        score = score + 1
+    else:
+        raw_input('\nWrong! Hit enter...')
 
-	count = count + 1
+    count = count + 1
 
 print '\nYour score is:', score
+
 
